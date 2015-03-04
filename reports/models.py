62396ee21988a87a9d9bcb49eeb495a98bf62fef
Francis Kung <kungfrancis@gmail.com>
2015-03-04 15:14:05 -0500

    Initial commit

---
+++ reports/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+
+class Supporter(models.Model):
+  class Meta:
+    db_table = 'supporters'
+    
+  created = models.DateTimeField(db_index=True)
+  modified = models.DateTimeField(db_index=True)
+  
+  postal_code = models.CharField(max_length=6, blank=True, null=True)
+
+class Campaign(models.Model):
+  class Meta:
+    db_table = 'campaigns'
+  
+  name = models.CharField(max_length=255)
+  campaign_type = models.CharField(max_length=25)
+  campaign_status = models.CharField(max_length=1)
+
+class Email(models.Model):
+  class Meta:
+    db_table = 'emails'
+    
+  supporter = models.ForeignKey(Supporter)
+  campaign = models.ForeignKey(Campaign)
+  
+  sent = models.DateTimeField(blank=True, null=True)
+  opened = models.DateTimeField(blank=True, null=True, db_index=True)
+  clicked = models.DateTimeField(blank=True, null=True, db_index=True)
+  hard_bounce = models.DateTimeField(blank=True, null=True, db_index=True)
+  soft_bounce = models.DateTimeField(blank=True, null=True, db_index=True)
+  unsub = models.DateTimeField(blank=True, null=True, db_index=True)
+
+class Action(models.Model):
+  class Meta:
+    db_table = 'actions'
+    
+  supporter = models.ForeignKey(Supporter)
+  campaign = models.ForeignKey(Campaign)
+  
+  timestamp = models.DateTimeField(db_index=True)
+  
+  browser = models.CharField(max_length=255, blank=True, null=True)
+  url = models.CharField(max_length=255, blank=True, null=True)
+  source = models.CharField(max_length=3, blank=True, null=True, db_index=True)
+
+class Query(models.Model):
+  class Meta:
+    db_table = 'queries'
+    
+  creator = models.ForeignKey('auth.User')
+  created = models.DateTimeField(auto_now_add=True)
+  
+  vis_emails = models.BooleanField(default=False)
+  vis_actions = models.BooleanField(default=False)
+  
---
+++ reports/settings.py
@@ -0,0 +1,95 @@
+"""
+Django settings for reports project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '^j2vd%xek$klf3i7aee9c5^l&oxv9sp0n+bcfk95_!a5@dmggc'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+TEMPLATE_DIRS = ('reports/templates')
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    
+    'reports',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'reports.urls'
+
+WSGI_APPLICATION = 'reports.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+
+
+# local_settings.py can be used to override environment-specific settings
+# like database and email that differ between development and production.
+try:
+    from local_settings import *
+except ImportError:
+    pass
+
+
